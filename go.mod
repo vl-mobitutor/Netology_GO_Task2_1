30db98c3c21d114a1e722b272aea0e0740eb910e
vl <vl@mobitutor.ru>
2020-06-17 14:48:02 +0300

    Basic functionality finished

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module Task1_1
+module github.com/vl-mobitutor/Netology_GO_Task2_1
 
 go 1.14
--- main.go
+++ main.go
@@ -1,18 +1,31 @@
 package main
+
 import (
 	"fmt"
 	"math"
 )
 
 func main() {
-	annuityCalc(1_000_000_00, 36, 20)
+	//Исходные данные для расчета параметров аннуитета
+	myLoanAmount := int64(1_000_000_00) //Сумма кредита в копейках
+	myLoanPeriod := 36 //Срок кредита в месяцах
+	myLoanPercent := 20 //Процентная ставка по кредиту в % годовых
+
+
+	myMonthlyPayment, myOverPayment, myTotalPayment := annuityCalc(myLoanAmount, myLoanPeriod, myLoanPercent)
+
+	//Вывод результатов расчета параметров аннуитета
+	fmt.Printf("Ежемесячныйй аннуитетный платеж: %d копеек \n", myMonthlyPayment)
+	fmt.Printf("Переплата по кредиту: %d копеек \n", myOverPayment)
+	fmt.Printf("Полная выплата по кредиту: %d копеек \n", myTotalPayment)
+
 }
 
 
 //ФУНКЦИЯ РАСЧЕТА АННУИТЕТА
 //Принимаемые параметры - сумма кредита в копейках, срок кредита в месяцах, ставка по кредиту в % годовых
 //Возвращаемые значения - ежемесячный аннуитетный платеж, переплата по кредиту, общая выплата по кредиту (все в копейках)
-func annuityCalc (loanSum, period, percent int64) int64 {
+func annuityCalc (loanAmount int64, loanPeriod, loanPercent int) (int64, int64, int64) {
 
 	var (
 		percentageMonthly, annuityRate float64
@@ -20,23 +33,21 @@ func annuityCalc (loanSum, period, percent int64) int64 {
 	)
 
 	//Расчет месячной процентной ставки
-	percentageMonthly = float64(percent) / 12 / 100
+	percentageMonthly = float64(loanPercent) / 12 / 100
 	percentageMonthly = math.Round(percentageMonthly*10000) / 10000 //Округление месячной ставки до 3-го знака
 
 	//Расчет коэффициента аннуитета
-	annuityRate = percentageMonthly * math.Pow((percentageMonthly + 1), float64(period)) / (math.Pow((percentageMonthly + 1), float64(period)) - 1)
+	annuityRate = percentageMonthly * math.Pow((percentageMonthly + 1), float64(loanPeriod)) / (math.Pow((percentageMonthly + 1), float64(loanPeriod)) - 1)
 	annuityRate = math.Round(annuityRate * 1_000_000) / 1_000_000 //Округление коэффиента до 6 знака
 
 	//Расчет суммы ежемесячного аннуитетного платежа
-	monthlyPayment = int64(float64(loanSum) * annuityRate)
+	monthlyPayment = int64(float64(loanAmount) * annuityRate)
 
 	//Расчет общей выплаты по кредиту
-	totalPayment = period * monthlyPayment
+	totalPayment = int64(loanPeriod) * monthlyPayment
 
 	//Расчет переплаты по кредиту
-	overPayment = totalPayment - loanSum
+	overPayment = totalPayment - loanAmount
 
-	fmt.Println(percentageMonthly, annuityRate, monthlyPayment, overPayment, totalPayment)
-
-	return 1
+	return monthlyPayment, overPayment, totalPayment
 }
