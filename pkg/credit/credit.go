b96ad52b94ac174b8b15662fa9ce5f4c8dcfe3f4
vl <vl@mobitutor.ru>
2020-06-17 19:56:05 +0300

    Packages structure created

--- pkg/credit/credit.go
+++ pkg/credit/credit.go
@@ -1,36 +1,13 @@
-package main
+package credit
 
-import (
-	"fmt"
-	"math"
-)
-
-func main() {
-	//Исходные данные для расчета параметров аннуитета
-	myLoanAmount := int64(1_000_000_00) //Сумма кредита в копейках
-	myLoanPeriod := 36 //Срок кредита в месяцах
-	myLoanPercent := 20 //Процентная ставка по кредиту в % годовых
-
-
-	myMonthlyPayment, myOverPayment, myTotalPayment := annuityCalc(myLoanAmount, myLoanPeriod, myLoanPercent)
-
-	//Вывод результатов расчета параметров аннуитета
-	fmt.Printf("Ежемесячныйй аннуитетный платеж: %d копеек \n", myMonthlyPayment)
-	fmt.Printf("Переплата по кредиту: %d копеек \n", myOverPayment)
-	fmt.Printf("Полная выплата по кредиту: %d копеек \n", myTotalPayment)
-
-}
-
+import "math"
 
 //ФУНКЦИЯ РАСЧЕТА АННУИТЕТА
 //Принимаемые параметры - сумма кредита в копейках, срок кредита в месяцах, ставка по кредиту в % годовых
 //Возвращаемые значения - ежемесячный аннуитетный платеж, переплата по кредиту, общая выплата по кредиту (все в копейках)
-func annuityCalc (loanAmount int64, loanPeriod, loanPercent int) (int64, int64, int64) {
+func Calculate (loanAmount int64, loanPeriod, loanPercent int) (monthlyPayment, overPayment, totalPayment int64) {
 
-	var (
-		percentageMonthly, annuityRate float64
-		monthlyPayment, totalPayment, overPayment int64
-	)
+	var percentageMonthly, annuityRate float64
 
 	//Расчет месячной процентной ставки
 	percentageMonthly = float64(loanPercent) / 12 / 100
@@ -49,5 +26,5 @@ func annuityCalc (loanAmount int64, loanPeriod, loanPercent int) (int64, int64, 
 	//Расчет переплаты по кредиту
 	overPayment = totalPayment - loanAmount
 
-	return monthlyPayment, overPayment, totalPayment
+	return
 }
